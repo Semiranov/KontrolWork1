d221cd6f184a6ae0e1aaae160b333ac845f513c8
Semiranov <semiranov@gmail.com>
2022-10-14 01:19:24 +0300

    add task code

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых <= 3 символа. Первоначальный массив можно ввести с клавиатуры,
+// либо задать на старте выполнения алгоритма. 
+
+string[] array ={"Hello", "Hi", "2", "world", ";-)", "-2", "1234", "*"}; //вводим массив
+
+int CountingStrings(string[] arr) // метод подсчета строк в массиве <=3
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)  
+    {
+        if (arr[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+string[] newArray = new string[CountingStrings(array)]; //вводим новый массив размерность count
+
+int j = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newArray[j] = array[i];
+        j++;
+    }
+}
+
+Console.WriteLine(string.Join(", ", newArray));  //выводим новый массив на экран
